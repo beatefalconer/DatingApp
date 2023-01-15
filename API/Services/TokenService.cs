e835035b44a60684e2d84ad9645e9e393142827a
bfalcone <beate_falconer@trimble.com>
2023-01-15 12:57:39 -0800

    fixed tokenService at end of 6

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -11,6 +11,10 @@ namespace API.Services
     public class TokenService : ITokenService
     {
         private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
         
         public string CreateToken(AppUser user)
         {
