e4e635aea06ac00e537b38b0dca233eac989823c
bfalcone <beate_falconer@trimble.com>
2023-02-03 12:04:22 -0800

    added missing lines to program.cs after .net7 upgr

--- API/Program.cs
+++ API/Program.cs
@@ -10,11 +10,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddCors();
+builder.Services.AddSignalR();
+
+// middleware
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("https://localhost:4200"));
